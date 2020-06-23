6822f9852c37c4fffa3d11d5f6703691ddef6315
Dave Laffan <dave.laffan@outlook.com>
2020-06-23 15:12:58 +0100

    Configure Flickety carousel.
    Double up on cbd product elements to force an infinite loop
    within the carousel. Don't believe Flickety has a way of doing this
    itself.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,18 @@
-$('.cbd-carousel-container').flickity();
+$(document).ready(function () {
+    // Set Flickity defaults
+    var fOptions = {
+        cellAlign: 'left',
+        contain: true,
+        wrapAround: true,
+        prevNextButtons: false,
+        pageDots: false,
+    };
+
+    // center carousel items on page load when width > 768px
+    if (matchMedia('screen and (min-width: 768px)').matches) {
+        fOptions.cellAlign = 'center';
+    }
+
+    // initialise flickety carousel
+    $('.cbd-carousel-container').flickity(fOptions);
+});
